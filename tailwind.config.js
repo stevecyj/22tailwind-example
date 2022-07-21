6e1eaf4228e2a2e38d44499c0063ac20b8bb1d46
stevecyj <ycsteven.tsao@gmail.com>
2022-07-21 11:27:58 +0800

    1st

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        lightBlue: "#1fb6ff",
+        tahiti: {
+          light: "#67e8f9",
+          DEFAULT: "#06b6d4",
+          dark: "#0e7490",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
